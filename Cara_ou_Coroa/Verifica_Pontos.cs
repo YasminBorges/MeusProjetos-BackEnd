3a8eadcec55e3b57a9b9ab77b0f7d403ae6d0e3a
YasminBorges <73182977+YasminBorges@users.noreply.github.com>
2023-03-07 12:00:57 -0300

    Add files via upload

---
+++ Cara_ou_Coroa/Verifica_Pontos.cs
@@ -0,0 +1,21 @@
+namespace Cara_ou_Coroa
+{
+    public class Verifica_Pontos
+    {
+         public void Verifica(int Pontos,int PontoSistema)
+          {
+               if(Pontos > PontoSistema)
+               {
+                    Console.WriteLine("VOCÊ GANHOU!!!!");
+               }
+               else if(Pontos < PontoSistema)
+               {
+                    Console.WriteLine("Você perdeu...");
+               }
+               else
+               {
+                    Console.WriteLine("Empate!!!");
+               }
+          }
+    }
+}
